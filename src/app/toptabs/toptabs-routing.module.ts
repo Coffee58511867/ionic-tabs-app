dd00a13712a114ce46e3ff791031603074d7657d
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-13 16:05:48 +0200

    Created app pages

--- src/app/toptabs/toptabs-routing.module.ts
+++ src/app/toptabs/toptabs-routing.module.ts
@@ -6,7 +6,32 @@ import { ToptabsPage } from './toptabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: ToptabsPage
+    component: ToptabsPage,
+    children : [
+
+      {
+        path: 'tab4',
+        loadChildren: () => import('../tab4/tab4.module').then( m => m.Tab4PageModule)
+      },
+      {
+        path: 'tab5',
+        loadChildren: () => import('../tab5/tab5.module').then( m => m.Tab5PageModule)
+      },
+      // {
+      //   path: 'tab3/:id',
+      //   loadChildren: () => import('../tab3/tab3.module').then( m => m.Tab3PageModule)
+      // },
+      {
+        path: '',
+        redirectTo: '/tabs/tab4',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/tabs/tab4',
+    pathMatch: 'full'
   }
 ];
 
